4447f747321a589ec86040b840203a7f494746c3
Djordje Todorovic <djordje.todorovic@htecgroup.com>
2024-08-14 14:11:48 +0200

    hw/riscv: Ensure proper ELF endian expectation
    load_elf_ram_sym has a `big_endian` paramater that notifies
    it if the loaded elf file is expected to be LE or BE. This
    paramater needs to be set based on target endianess to ensure
    proper elf loading.

--- hw/riscv/boot.c
+++ hw/riscv/boot.c
@@ -158,7 +158,7 @@ target_ulong riscv_load_firmware(const char *firmware_filename,
 
     if (load_elf_ram_sym(firmware_filename, NULL, NULL, NULL,
                          &firmware_entry, NULL, &firmware_end, NULL,
-                         0, EM_RISCV, 1, 0, NULL, true, sym_cb) > 0) {
+                         TARGET_BIG_ENDIAN, EM_RISCV, 1, 0, NULL, true, sym_cb) > 0) {
         return firmware_end;
     }
 
